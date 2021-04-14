4680cf7cae1a5c6f67a7b33a1cf9fa184343e8b4
Slayter <slayter007@gmail.com>
2021-04-14 18:23:24 +0300

    Add transformers

--- app/Http/Controllers/Auth/MeController.php
+++ app/Http/Controllers/Auth/MeController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Transformers\Users\UserTransformer;
 use Illuminate\Http\Request;
 
 class MeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth:api']);
+    }
+
     public function __invoke(Request $request)
     {
-        dd($request);
+        return fractal()
+            ->item($request->user())
+            ->transformWith(new UserTransformer())
+            ->toArray();
     }
 }
