1a2bf1a929899f89db9ae997c81c0b22d56c42f4
Slayter <slayter007@gmail.com>
2021-04-15 16:42:49 +0300

    Update api

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=>'auth', 'namespace'=>'App\Http\Controllers\Auth'], function() {
     Route::post('signin', 'SignInController');
     Route::post('signout', 'SignOutController');
-    Route::get('user', 'MeController');
+    Route::get('user', 'UserController');
 });
