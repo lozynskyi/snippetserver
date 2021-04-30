b28fecfbad6fb8dd90fe704411c5597713f8a5f2
Slayter <slayter007@gmail.com>
2021-04-30 16:00:08 +0300

    Add presenter

--- app/Http/Controllers/User/PersonController.php
+++ app/Http/Controllers/User/PersonController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Transformers\Users\PublicUserTransformer;
 
 class PersonController extends Controller
 {
     public function show(User $user)
     {
-        dd($user);
+        return fractal()
+            ->item($user)
+            ->transformWith(new PublicUserTransformer())
+            ->toArray();
     }
 }
