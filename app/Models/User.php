ca95bc34add03fd00b8a1492dc6d8e9a40ead49e
Slayter <slayter007@gmail.com>
2021-05-04 23:53:01 +0300

    Add user password update

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
@@ -42,6 +43,19 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @param $password
+     */
+    public function setPasswordAttribute($password): void
+    {
+        if (trim($password) === '')
+        {
+            return;
+        }
+
+        $this->attributes['password'] = Hash::make($password);
+    }
 
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
