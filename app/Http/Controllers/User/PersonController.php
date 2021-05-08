f06ad6b36b591302d0e5de62471650d5cacc5ea5
Slayter <slayter007@gmail.com>
2021-05-08 23:39:55 +0300

    Add controller update

--- app/Http/Controllers/User/PersonController.php
+++ app/Http/Controllers/User/PersonController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Transformers\Users\PublicUserTransformer;
+use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
@@ -15,13 +16,23 @@ class PersonController extends Controller
             ->transformWith(new PublicUserTransformer())
             ->toArray();
     }
-    public function update(User $user)
+
+    public function update(User $user, Request $request)
     {
+        //dd($user->id);
         $this->authorize('as', $user);
 
-        return fractal()
-            ->item($user)
-            ->transformWith(new PublicUserTransformer())
-            ->toArray();
+        dd($request->only('email', 'name', 'username'));
+
+        $this->validate($request, [
+            'email' => 'required|email|unique:users,email,'.$request->user()->id,
+            'username' => 'required|alpha_dash|unique:users,username,'.$request->user()->id,
+            'name' => 'required',
+            'password' => 'nullable|min:6'
+        ]);
+
+        $user->update(
+            $request->only('email', 'name', 'username')
+        );
     }
 }
