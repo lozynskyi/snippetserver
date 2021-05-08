df0597c178d61e5d2d652fa412ab0ea33db62153
Slayter <slayter007@gmail.com>
2021-05-09 01:34:54 +0300

    Add person update

--- app/Http/Controllers/User/PersonController.php
+++ app/Http/Controllers/User/PersonController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Transformers\Users\PublicUserTransformer;
+use App\Transformers\Users\UserTransformer;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class PersonController extends Controller
 {
@@ -17,22 +20,40 @@ class PersonController extends Controller
             ->toArray();
     }
 
-    public function update(User $user, Request $request)
+    public function update(User $user, Request $request): \Illuminate\Http\JsonResponse
     {
-        //dd($user->id);
-        $this->authorize('as', $user);
+        try {
 
-        dd($request->only('email', 'name', 'username'));
+            $this->authorize('as', $user);
 
-        $this->validate($request, [
-            'email' => 'required|email|unique:users,email,'.$request->user()->id,
-            'username' => 'required|alpha_dash|unique:users,username,'.$request->user()->id,
-            'name' => 'required',
-            'password' => 'nullable|min:6'
-        ]);
+            $this->validate($request, [
+                'email' => 'required|email|unique:users,email,'.$request->user()->id,
+                'username' => 'required|alpha_dash|unique:users,username,'.$request->user()->id,
+                'name' => 'required',
+                'password' => 'nullable|min:6'
+            ]);
 
-        $user->update(
-            $request->only('email', 'name', 'username')
-        );
+            $user->update(
+                $request->only('email', 'name', 'username')
+            );
+
+        } catch (AuthorizationException $e) {
+            return response()->json([
+                'errors' => [
+                    'action' => ['Authenticated user is not authorized to do this action.']
+                ]
+            ], 403);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'errors' => [
+                    $e->errors()
+                ]
+            ], 422);
+        }
+
+        return fractal()
+            ->item($request->user())
+            ->transformWith(new UserTransformer())
+            ->toArray();
     }
 }
