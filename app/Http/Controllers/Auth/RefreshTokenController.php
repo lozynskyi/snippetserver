35b090c060c6c1d467df5657c0e7fdbcf5a649ac
Slayter <slayter007@gmail.com>
2021-05-09 02:55:05 +0300

    Add refresh tocken

--- app/Http/Controllers/Auth/RefreshTokenController.php
+++ app/Http/Controllers/Auth/RefreshTokenController.php
@@ -3,12 +3,36 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RefreshTokenController extends Controller
 {
-    public function __invoke(Request $request): array
+
+    public function __invoke(Request $request)
     {
-        return [];
+        try {
+            return $this->respondWithToken(auth()->refresh());
+        } catch (Exception $e) {
+            return response()->json([
+                'errors' => [
+                    $e->getMessage()
+                ]
+            ], 422);
+        }
+    }
+
+    /**
+     * @param string $token
+     * @return array
+     */
+    protected function respondWithToken(string $token): array
+    {
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ];
     }
 }
